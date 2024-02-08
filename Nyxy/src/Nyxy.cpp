7fff7f4eb46316735810b8954d5339433e7e63e3
Makosai <kristopher.ali.dev@gmail.com>
2024-02-07 21:15:59 -0500

    Create a HelloWorld function for the engine.

---
+++ Nyxy/src/Nyxy.cpp
@@ -0,0 +1,8 @@
+#include "Nyxy.h"
+#include <stdio.h>
+
+namespace Nyxy {
+	void HelloWorld() {
+		printf("Hello world!\n");
+	}
+}
---
+++ Nyxy/src/Nyxy.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace Nyxy {
+	__declspec(dllexport) void HelloWorld();
+}
