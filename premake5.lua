997c195138caf33a73b96dd8ab571fc80bff3823
Makosai <kristopher.ali.dev@gmail.com>
2024-02-09 19:38:50 -0500

    Configure Premake5.

---
+++ premake5.lua
@@ -0,0 +1,101 @@
+workspace "Nyxy"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+	
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Nyxy"
+	location "Nyxy"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"NE_PLATFORM_WINDOWS",
+			"NE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Simul")
+		}
+
+	filter "configurations:Debug"
+		defines "NE_DEBUG"
+		symbols "On"
+	filter "configurations:Release"
+		defines "NE_RELEASE"
+		optimize "On"
+	filter "configurations:Dist"
+		defines "NE_DIST"
+		optimize "On"
+
+project "Simul"
+	location "Simul"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include",
+		"Nyxy/src"
+	}
+
+	links
+	{
+		"Nyxy"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"NE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "NE_DEBUG"
+		symbols "On"
+	filter "configurations:Release"
+		defines "NE_RELEASE"
+		optimize "On"
+	filter "configurations:Dist"
+		defines "NE_DIST"
+		optimize "On"
